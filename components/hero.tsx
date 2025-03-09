414320dbbe3852b3380d91d1a1fe9bf8f3bba377
PittPrat <pittalaprathik@gmail.com>
2025-03-09 07:02:38 -0700

    video addition

--- components/hero.tsx
+++ components/hero.tsx
@@ -39,7 +39,7 @@ export default function Hero() {
           playsInline 
           className="w-full h-full object-cover"
         >
-          <source src="/videos/Video.mp4" type="video/mp4" />
+          <source src="/videos/Video6.mp4" type="video/mp4" />
         </video>
       </div>
       
