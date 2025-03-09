522cf88dd48b3af04fc83f8371d536ab59d36d8d
PittPrat <pittalaprathik@gmail.com>
2025-03-09 03:19:29 -0700

    config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,54 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
+
+let userConfig = undefined
+try {
+  userConfig = await import('./v0-user-next.config')
+} catch (e) {
+  // ignore error
+}
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   assetPrefix: '/',
   basePath: '',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
-      unoptimized: true,
+    unoptimized: true,
   },
   experimental: {
-      appDir: true,
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+    appDir: true,
   },
-};
+}
+
+mergeConfig(nextConfig, userConfig)
+
+function mergeConfig(nextConfig, userConfig) {
+  if (!userConfig) {
+    return
+  }
+
+  for (const key in userConfig) {
+    if (
+      typeof nextConfig[key] === 'object' &&
+      !Array.isArray(nextConfig[key])
+    ) {
+      nextConfig[key] = {
+        ...nextConfig[key],
+        ...userConfig[key],
+      }
+    } else {
+      nextConfig[key] = userConfig[key]
+    }
+  }
+}
 
 export default nextConfig;
