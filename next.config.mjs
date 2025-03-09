2da2a5be202bb435bfeacc3484807b05f5ba041a
PittPrat <pittalaprathik@gmail.com>
2025-03-09 01:31:12 -0800

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 export default {
   output: 'export',
-  basePath: isProd ? '/pittprat.github.io' : '',
-  assetPrefix: isProd ? '/pittprat.github.io/' : '/',
+  basePath: '',
+  assetPrefix: './',
   trailingSlash: true,
   images: {
     unoptimized: true,
